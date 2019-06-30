9b67b9403eb68eff602233e3403c6b2d50e9cdb2
axb2035 <contact@blendertraining.com>
2019-07-01 06:34:53 +1000

    Update setup.py
    Fix install package info

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_chase',
       version='0.0.1',
+      packages=['gym_chase', 'gym_chase.envs'],
       install_requires=['gym']  # Add any other dependencies Chase needs
 )
