5299002376565e03ca9ef588d1bdfe1d06babc58
axb2035 <axb2035@gmail.com>
2022-11-02 08:23:53 +1030

    Fix directions matrix
    :face palm:

--- gym_chase/envs/chase_env.py
+++ gym_chase/envs/chase_env.py
@@ -142,15 +142,15 @@ class ChaseEnv(gym.Env):
         self.action_space = spaces.Discrete(9,)
 
         self.action_to_direction = {
-            1: np.array([-1, 1]),
-            2: np.array([0, 1]),
+            1: np.array([1, -1]),
+            2: np.array([1, 0]),
             3: np.array([1, 1]),
-            4: np.array([-1, 0]),
+            4: np.array([0, -1]),
             5: np.array([0, 0]),
-            6: np.array([1, 0]),
+            6: np.array([0, 1]),
             7: np.array([-1, -1]),
-            8: np.array([0, -1]),
-            9: np.array([1, -1]),
+            8: np.array([-1, 0]),
+            9: np.array([-1, 1]),
         }
 
     def step(self, action):
