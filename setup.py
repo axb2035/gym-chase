283bbf70711ea8df07f09f00f4e17902a5e5c017
axb2035 <axb2035@gmail.com>
2022-11-27 07:25:44 +1030

    Make gym-chase editable and update to gymnasium

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     name="gym_chase",
     version="0.0.2",
     packages=["gym_chase", "gym_chase.envs"],
-    install_requires=["gym"],  # Add any other dependencies Chase needs
+    install_requires=["gymnasium"],  # Add any other dependencies Chase needs
 )
