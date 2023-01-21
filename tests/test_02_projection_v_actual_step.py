dfc31dbda838d45aeb459782ef55d5b609970a75
axb2035 <axb2035@gmail.com>
2023-01-21 12:24:03 +1030

    Update code quality

--- tests/test_02_projection_v_actual_step.py
+++ tests/test_02_projection_v_actual_step.py
@@ -1,8 +1,9 @@
-import gymnasium as gym
-import pytest
-import numpy as np
-from tqdm import tqdm
 import json
+
+import gymnasium as gym
+import numpy as np
+import pytest
+from tqdm import tqdm
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -21,14 +22,20 @@ def state_to_json(state):
     Returns:
         A JSON string representation of the state.
     """
-    return json.dumps(state, sort_keys=True, cls=NumpyEncoder,)
+    return json.dumps(
+        state,
+        sort_keys=True,
+        cls=NumpyEncoder,
+    )
 
 
 @pytest.fixture
 def env() -> gym.Env:
     """Create a new unwrapped instance of the Chase-v1 environment."""
     chase = gym.make("gym_chase:Chase-v1")
-    return chase.unwrapped  # As using projection, we need to use the unwrapped environment.
+    return (
+        chase.unwrapped
+    )  # As using projection, we need to use the unwrapped environment.
 
 
 def test_chase_environment(env: gym.Env) -> None:
@@ -53,7 +60,9 @@ def test_chase_environment(env: gym.Env) -> None:
                 # Take a projected action and observe the result.
                 state, reward, terminated, truncated, info = env.step(action, True)
                 # Validate the agent hasn't moved comparing against the initial state.
-                assert state_to_json(state) != initial_state_json, "Projected state is not the same as initial state."
+                assert (
+                    state_to_json(state) != initial_state_json
+                ), "Projected state is not the same as initial state."
 
             # Then take a random action.
             action = env.action_space.sample()
